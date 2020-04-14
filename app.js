76c3fb7c8ecf32c86357c694aafe45dcfb7017a4
Yulia Tarima <yulia.tarima@gmail.com>
2020-04-13 21:06:35 -0700

    Managin orders with Mongoose; env vars work

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
-mongoose.connect("mongodb+srv://rest-shop:rest-shop@node-rest-shop-7aywh.mongodb.net/test?retryWrites=true&w=majority",
-
+mongoose.connect(
+  // "mongodb+srv://rest-shop:rest-shop@node-rest-shop-7aywh.mongodb.net/test?retryWrites=true&w=majority",
+  "mongodb+srv://" + process.env.MONGO_ATLAS_USER + ":" + process.env.MONGO_ATLAS_PW + "@node-rest-shop-7aywh.mongodb.net/test?retryWrites=true&w=majority",
     // "mongodb+srv://rest-shop:@node-rest-shop-7aywh.mongodb.net/test?retryWrites=true&w=majority",
     { 
         // user: process.env.MONGO_ATLAS_USER, 
         // pass: process.env.MONGO_ATLAS_PW,
-        useNewUrlParser :true,
+        useNewUrlParser: true,
         useUnifiedTopology: true
   }
 )
 .then(() => console.log('MongoDB connected...'))
 .catch(err => { // if error we will be here
+    console.error('process.env.MONGO_ATLAS_USER:', process.env.MONGO_ATLAS_USER);
     console.error('App starting error:', err.stack);
     process.exit(1);
 });
+
+mongoose.Promise = global.Promise;
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
